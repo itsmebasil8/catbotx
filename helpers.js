7d0755ff6753702f965ff188e7de4c7567f542b4
itsmebasil8 <92202940+itsmebasil8@users.noreply.github.com>
2021-12-02 21:06:36 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ *catbot*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *catbot*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ *catbot*:  ```" + msg + "```"
 }
 
 
